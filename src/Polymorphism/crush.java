247a4161aeeb837b5a4df25f78b3dcd9f29e3ad5
remultajustinekyle <144421878+remultajustinekyle@users.noreply.github.com>
2023-11-24 16:21:11 +0800

    Update crush.java

--- src/Polymorphism/crush.java
+++ src/Polymorphism/crush.java
@@ -1,17 +1,1 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Polymorphism;
 
-/**
- *
- * @author CRISTINA P. SUELLO
- */
-class crush {
-
-    void secret() {
-        
-    }
-    
-}
